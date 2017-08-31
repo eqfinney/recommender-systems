dcb527b1f258812adabea25d7c5896ca33a1f2d6
Emily Quinn Finney <eqfinney@ucdavis.edu>
2017-08-30 20:53:20 -0400

    updated hypothesis test

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -4,19 +4,23 @@
 #
 
 import pandas as pd
-from hypothesis import given
+from hypothesis import given, assume
 from hypothesis import strategies as st
 
 
 import charities_toyscript as ct
 
-@given(st.lists, st.lists)
+keyword = st.lists(st.floats(allow_nan=False, min_value=1.e-50, max_value=1.e50), min_size=1)
+df_keyword = st.from_type(pd.DataFrame())
+
+@given(keyword, keyword)
 def test_calculate_similarity(x, y):
-    result = ct.calculate_similarity(x, y)
-    result = ct.calculate_similarity(y, x)
+    assume(len(x) == len(y))
+    result1 = ct.calculate_similarity(x, y)
+    result2 = ct.calculate_similarity(y, x)
     assert result1 == result2
 
-@given(st.lists, st.from_type(pd.DataFrame()))
+@given(keyword, st.from_type(pd.DataFrame()))
 def test_find_best_value(x, y):
     result = ct.find_best_match(x, y)
     assert result.isinstance(pd.Series)
