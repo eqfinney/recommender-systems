4419adfa421414032701404b53850518daf5f1ab
Emily Quinn Finney <eqfinney@ucdavis.edu>
2017-10-31 13:20:39 -0400

    fixing up tag location with @ellykuhlman

--- charity_scraping.py
+++ charity_scraping.py
@@ -4,10 +4,14 @@
 #
 
 import sys
-sys.path.insert(0, '/home/equinney/github/web-scraping')
-import web_crawler_main_class as webs
-sys.path.insert(0, '/home/eqfinney/github/verbal-infusions')
-#from hteaml_parser import read_soup
+#sys.path.insert(0, '/home/equinney/github/web-scraping')
+#import web_crawler_main_class as webs
+sys.path.insert(0, '/home/equinney/github/verbal-infusions')
+from hteaml_parser import read_soup
+import pandas as pd
+import re
+import time
+import bs4
 
 
 def scrape_website(url_list):
@@ -27,7 +31,7 @@ def scrape_website(url_list):
     loop.close()
 
 
-def locate_descriptive_text(structured_page, tag_pattern, tag_names, filename):
+def locate_tags(structured_page, tag_pattern, contains):
     """
     Given a page structured in a Beautiful Soup format, returns all descriptive text on page
     :param structured_page: list of web pages, structured in Beautiful Soup format
@@ -36,26 +40,51 @@ def locate_descriptive_text(structured_page, tag_pattern, tag_names, filename):
     :param filename: string, the name of the file to which to write the text
     :return: nothing, but should write a corpus of text to file
     """
-    with open(filename, 'a') as f:
         # identify all descriptions on web page matching the pattern in the PageScraper object
-        prod_description = structured_page.find_all('div', class_=re.compile(tag_pattern))
-        # then go through them
-        # this method is going to depend on the structure of the web page
-        # so I'm not sure how I would generalize it
-        for prod in prod_description:
-            if prod['class'][0] == tag_names[0]:
-                for element in prod:
-                    if element.string:
-                        f.write(element.string)
-            elif prod['class'][0] == tag_names[1]:
-                if prod.ul:
-                    for child in prod.ul.children:
-                        if child:
-                            if child.string:
-                                f.write(child.string)
+    tags = structured_page.find_all('div', class_=re.compile(tag_pattern))
+    #print(len(tags))
+    #import pprint
+    for i, tag in enumerate(tags):
+        if contains in tag.text:
+            #print(i)
+            #pprint.pprint(tag)
+            yield tag
+
+def locate_charity_info(tag, tag_names):
+    # locate mission info
+    queue = list(tag.children)
+    while len(queue) != 0:
+        element = queue.pop(0)
+        if type(element) == bs4.element.Tag:
+            if element.name in tag_names[0]:
+                # header title
+                print(element.string.strip())
+            elif element.name in tag_names[1]:
+                # contents
+                if element.string:
+                    print(element.string.strip())
+                elif element.find_all(id="orgSiteLink"):
+                    print(element.a['href'].strip())
+                else:
+                    print(element.text.strip())
             else:
-                pass
+                for sublist in list(element.children):
+                    queue.append(sublist)
+
 
 
 if __name__ == '__main__':
-    scrape_website(['https://www.charitynavigator.org/index.cfm?bay=search.alpha'])
+    #scrape_website(['https://www.charitynavigator.org/index.cfm?bay=search.alpha'])
+    """with open('cn_working.html', 'r') as f:
+        new_file = f.read()
+    df = pd.read_html(new_file)#, re.compile('(cn-appear*|accordion-item-bd*)'))
+    for frame in df:
+        print("next frame is:")
+        print(frame)
+        """
+    page = read_soup('cn_working.html')
+    contact_info_generator = locate_tags(page[0], '(^rating$)', 'Charity Contact Info')
+    mission_generator = locate_tags(page[0], '(^summaryBox cn-accordion-rating$)', 'Mission')
+    contact_info_tag = next(contact_info_generator)
+    mission_tag = next(mission_generator)
+    locate_charity_info(contact_info_tag, ('h1', 'p'))
